4b841b8b159157f6d574816e96323ce49330f05f
Mariela Plaza <mariela.plaza92@gmail.com>
2023-03-25 20:41:45 +0100

    Create and use PokemonCard component

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="sprite" class="pokemon-img" :alt="name" />
+    <div>
+      <h5 class="pokemon-name">{{ formattedName }}</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    formattedName() {
+      return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+    },
+    entryNumber() {
+      const separatedString = this.url.split("/");
+      return separatedString[separatedString.length - 2];
+    },
+    sprite() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.entryNumber}.png`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pokemon-img {
+  width: 75%;
+  align-self: center;
+}
+.pokemon-name {
+  padding-bottom: 1rem;
+}
+</style>
--- src/views/PokemonTeamView.vue
+++ src/views/PokemonTeamView.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="my-team">
-    <ul v-for="pokemon in pokemonList" :key="pokemon.id">
-      <li>{{ pokemon.name }}</li>
-    </ul>
+  <div class="container text-center">
+    <div class="row">
+      <ul
+        class="col-12 col-sm-6 col-md-4 col-xl-3"
+        v-for="pokemon in pokemonList"
+        :key="pokemon.name"
+      >
+        <pokemon-card :name="pokemon.name" :url="pokemon.url"></pokemon-card>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import PokemonCard from "@/components/PokemonCard.vue";
 
 export default {
   name: "PokemonTeam",
+  components: { PokemonCard },
   computed: {
     ...mapGetters({ pokemonList: "getAllPokemon" }),
   },
