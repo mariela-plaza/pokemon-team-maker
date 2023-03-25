1f325bc772d42145fb6c5c818d2a3c2cee5c1cf5
Mariela Plaza <mariela.plaza92@gmail.com>
2023-03-25 21:39:36 +0100

    Remove navbar from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <nav><router-link to="pokemon-team">My Team</router-link></nav>
   <router-view />
 </template>
 
@@ -12,16 +11,7 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin-top: 3rem;
 }
 </style>
