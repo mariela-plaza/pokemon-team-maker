6929d6ef5f5a07c451a289648e0b96f1ceea9c32
Mariela Plaza <mariela.plaza92@gmail.com>
2023-03-25 18:00:23 +0100

    Improve orginal router implementation

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,13 @@ import PokemonTeamView from "../views/PokemonTeamView";
 
 const routes = [
   {
+    path: "/",
+    redirect: "/pokemon-team",
+  },
+  {
     path: "/pokemon-team",
     name: "pokemon-team",
     component: PokemonTeamView,
-  },
-  {
-    path: "/",
-    redirect: "/pokemon-team",
   },
 ];
 
