9a7c61e3b98af7964a3726950f7b97a94aac36f8
Mariela Plaza <mariela.plaza92@gmail.com>
2023-03-25 17:03:23 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <nav><router-link to="pokemon-team">My Team</router-link></nav>
   <router-view />
 </template>
 
---
+++ src/views/PokemonTeamView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="my-team"></div>
+</template>
+
+<script>
+export default {
+  name: "PokemonTeam",
+};
+</script>
