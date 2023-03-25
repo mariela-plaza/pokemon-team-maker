ba40b684e433bf016e1ffb9d72087dc9088cbdbf
Mariela Plaza <mariela.plaza92@gmail.com>
2023-03-25 18:00:53 +0100

    Use our pokemon store in PokemonTeamView

--- src/views/PokemonTeamView.vue
+++ src/views/PokemonTeamView.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="my-team"></div>
+  <div class="my-team">
+    <ul v-for="pokemon in pokemonList" :key="pokemon.id">
+      <li>{{ pokemon.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   name: "PokemonTeam",
+  computed: {
+    ...mapGetters({ pokemonList: "getAllPokemon" }),
+  },
+  methods: {
+    ...mapActions(["fetchPokemonList"]),
+  },
+  mounted() {
+    this.fetchPokemonList();
+  },
 };
 </script>
